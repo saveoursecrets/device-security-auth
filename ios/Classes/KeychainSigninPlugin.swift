fa503ca77c1546428b89050683d80b60eb70c668
muji <muji@tmpfs.org>
2023-10-30 12:40:11 +0800

    Prepare to update iOS implementation.

--- ios/Classes/KeychainSigninPlugin.swift
+++ ios/Classes/KeychainSigninPlugin.swift
@@ -33,6 +33,47 @@ public class KeychainSigninPlugin: NSObject, FlutterPlugin {
         }
         switch method {
             case .saveAccountPassword(let account):
+                var accessControlError: Unmanaged<CFError>?
+                defer {
+                    accessControlError?.release()
+                }
+
+                guard let accessControl = SecAccessControlCreateWithFlags(
+                    nil,
+                    kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly as CFString,
+                    [.userPresence],
+                    &accessControlError
+                ) else {
+                    if let error = accessControlError {
+                        let flutterError = FlutterError(
+                            code: "access_control_error",
+                            message: "access control error: \(error)",
+                            details: nil)
+                        result(flutterError)
+                    }
+                    return
+                }
+                
+                var keychainQuery = [
+                    kSecAttrAccessControl: accessControl,
+                    kSecClass: kSecClassGenericPassword,
+                    kSecAttrService: account.serviceName,
+                    kSecAttrAccount: account.accountName,
+                    kSecValueData: account.password.data(using: .utf8)!
+                ] as [String: Any];
+
+                let status = SecItemAdd(keychainQuery as CFDictionary, nil)
+                if status == errSecSuccess {
+                    result(true)
+                } else {
+                    let flutterError = FlutterError(
+                        code: "save_account_password_error",
+                        message: "Error saving password to keychain: \(status)",
+                        details: nil)
+                    result(flutterError)
+                }
+                
+                /*
                 let keychainQuery: [String: Any] = [
                     // NOTE: must use kSecClassGenericPassword 
                     // NOTE: rather than kSecClassInternetPassword
@@ -52,7 +93,33 @@ public class KeychainSigninPlugin: NSObject, FlutterPlugin {
                         details: nil)
                     result(flutterError)
                 }
+                */
             case .readAccountPassword(let account):
+                let query: [String: Any] = [
+                    kSecClass as String: kSecClassGenericPassword,
+                    kSecAttrService as String: account.serviceName,
+                    kSecAttrAccount as String: account.accountName,
+                    kSecReturnData as String: kCFBooleanTrue,
+                    kSecMatchLimit as String: kSecMatchLimitOne
+                ];
+                
+                var passwordData: AnyObject?
+                let status = SecItemCopyMatching(
+                    query as CFDictionary, &passwordData)
+                if status == errSecSuccess, 
+                    let retrievedData = passwordData as? Data,
+                    let retrievedPassword = String(
+                        data: retrievedData, encoding: .utf8) {
+                    result(retrievedPassword)
+                } else {
+                    let flutterError = FlutterError(
+                        code: "read_account_password_error",
+                        message: "error reading password from keychain: \(status)",
+                        details: nil)
+                    result(flutterError)
+                }
+                
+                /*
                 let context = LAContext()
                 var error: NSError?
 
@@ -123,6 +190,7 @@ public class KeychainSigninPlugin: NSObject, FlutterPlugin {
                         result(flutterError)
                     }
                 }
+                */
             case .setLocalizationModel(let model):
                 if let model {
                     localizationModel = model
